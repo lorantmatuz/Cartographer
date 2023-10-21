49dcdb126d85699d282f5542985e2b734ed35521
Matuz <matuzlorant20@gmail.com>
2023-10-21 02:13:06 +0200

    Main driver modified

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,42 @@
-//const table = document.querySelector('table');
-
-const spring  = document.querySelector('#spring');
+/* const spring  = document.querySelector('#spring');
 const summer  = document.querySelector('#summer');
 const fall    = document.querySelector('#fall');
 const winter  = document.querySelector('#winter');
 
 const totalPoint = document.querySelector('#totalPoint');
+ */
+/* Button listeners */
+
+document.querySelector('#rotateButton').addEventListener('click', () => {
+  elements.current.rotate();
+  elements.current.print();
+});
+
+document.querySelector('#mirrorButton').addEventListener('click', () => {
+  elements.current.mirror();
+  elements.current.print();
+});
 
 
+/* Shape update */
+
+function removeImages() {
+  for (let i = 0; i < 9; i++) {
+    while(elementTableTds[i].firstChild) {
+      elementTableTds[i].removeChild(elementTableTds[i].firstChild);
+    }
+  }
+}
 
 
+/* Driver code  */
 
-createTable();
-initTable([[2,2], [4,9], [6,4], [9,10], [10,6]], 
-          'resources/fields/mountain.png');
+const table = new Table();
+table.createTable();
 
-callDelegate();
+delegateAll();
 
-/* Driver code */
 
-currElement = getRandomElement();
-print(currElement);
+const elements = new Elements();
+elements.next().print();
 
