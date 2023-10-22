c231ad5e590d6e64e8a0c78c2580f348d4890305
Matuz <matuzlorant20@gmail.com>
2023-10-22 18:55:08 +0200

    Game class added.

--- scripts/main.js
+++ scripts/main.js
@@ -5,38 +5,7 @@ const winter  = document.querySelector('#winter');
 
 const totalPoint = document.querySelector('#totalPoint');
  */
-/* Button listeners */
-
-document.querySelector('#rotateButton').addEventListener('click', () => {
-  elements.current.rotate();
-  elements.current.print();
-});
-
-document.querySelector('#mirrorButton').addEventListener('click', () => {
-  elements.current.mirror();
-  elements.current.print();
-});
-
-
-/* Shape update */
-
-function removeImages() {
-  for (let i = 0; i < 9; i++) {
-    while(elementTableTds[i].firstChild) {
-      elementTableTds[i].removeChild(elementTableTds[i].firstChild);
-    }
-  }
-}
-
 
 /* Driver code  */
 
-const table = new Table();
-table.createTable();
-
-delegateAll();
-
-
-const elements = new Elements();
-elements.next().print();
-
+new Game();
