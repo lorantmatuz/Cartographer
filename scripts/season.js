904f19b0a3bb0150b1c8a54c35042cfcf3ddd170
matuzl20 <matuzlorant20@gmail.com>
2023-11-01 21:56:52 +0100

    Season updated with marking dots

--- scripts/season.js
+++ scripts/season.js
@@ -2,41 +2,50 @@ class Season {
   totalPointSpan = document.querySelector('#totalPoint span');
   timeInSeasonSpan = document.querySelector('#timeInSeason span');
   currentSeasonSpan = document.querySelector('#currentSeason span');
-  constructor(season, points) {
-    this.season = season;
+  missionTdDivs = document.querySelectorAll('#currentSeason table td div');
+  constructor(seasons) {
+    this.seasons = seasons;
+    console.log("SEASON CREATED",this.seasons.indices);
+    this.seasonName = seasons.seasonList[seasons.index];
     this.duration = 7;
-    this.totalPoint = points;
-    this.pointInSeasonSpan = document.querySelector('#' + this.season + ' span');
+    this.pointInSeasonSpan = document.querySelector('#' + this.seasonName + ' span');
     this.init();
   }
 
   init() {
-    this.currentSeasonSpan.innerHTML = this.season;
+    this.currentSeasonSpan.innerHTML = this.seasonName;
+    for (let i = 0; i < this.seasons.seasonList.length; i++) {
+      if(this.seasons.indices.includes(i)) {
+        this.missionTdDivs[i].className = 'circle filled-' + this.seasonName;
+      } else {
+        this.missionTdDivs[i].className = 'circle empty';
+      }
+    }
     this.timeDecrease(0);
     this.addPoint(0);
   }
 
   addPoint(point) {
-    this.totalPoint += point;
     this.pointInSeasonSpan.innerHTML = point;
-    this.totalPointSpan.innerHTML = this.totalPoint;
-    console.log(this.totalPoint);
+    this.seasons.totalPoint += point;
+    this.totalPointSpan.innerHTML = this.seasons.totalPoint;
   }
 
   // todo
   timeDecrease(duration) {
-    this.duration -= duration;
+    this.duration = Math.max(this.duration - duration, 0);
     this.timeInSeasonSpan.innerHTML = this.duration;
   }
 
   isNotOver() {
     return this.duration > 0;
   }
-};
+}
 
 class Seasons {
-  seasons = ["Spring", "Summer", "Fall", "Winter"];
+  seasonList = ["Spring", "Summer", "Fall", "Winter"];
   current = null;
+  indices = [];
   totalPoint = 0;
   constructor(game) {
     this.game = game;
@@ -45,14 +54,17 @@ class Seasons {
   }
 
   isNotOver() {
-    return this.index < this.seasons.length;
+    return this.index < this.seasonList.length || this.current.isNotOver();
   }
 
   next() {
     if(this.current) {
       this.game.elements.shuffle();
-      this.totalPoint = this.current.totalPoint;
+      this.indices.length = 0;
     }
-    this.current = new Season(this.seasons[this.index++], this.totalPoint);
+    this.indices.push(this.index);
+    this.indices.push((this.index + 1) % this.seasonList.length);
+    this.current = new Season(this);
+    ++this.index;
   }
 }
